ebfceba55e0b5f96d4345dd41abf853c97c58248
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2022-10-07 07:04:44 +0700

    Create 20221006.cpp

---
+++ 20221006.cpp
@@ -0,0 +1,158 @@
+
+2022-10-06 21:35:44.730415 - C tpye: 0108466678-mrthanh@ubot.vn-T8- So hoa don tren TCT: 8
+2022-10-06 21:36:22.652899 - K tpye: 0108466678-mrthanh@ubot.vn-T8- So hoa don tren TCT: 23
+2022-10-06 21:36:50.224054 - C tpye: 0108466678-mrthanh@ubot.vn-T9- So hoa don tren TCT: 10
+2022-10-06 21:37:06.799326 - K tpye: 0108466678-mrthanh@ubot.vn-T9- So hoa don tren TCT: 13
+2022-10-06 21:37:25.446715 - C tpye: 0108543932-mrthanh@ubot.vn-T8- No inv
+2022-10-06 21:37:45.003231 - K tpye: 0108543932-mrthanh@ubot.vn-T8- So hoa don tren TCT: 11
+2022-10-06 21:37:53.635462 - C tpye: 0108543932-mrthanh@ubot.vn-T9- No inv
+2022-10-06 21:38:01.262099 - K tpye: 0108543932-mrthanh@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-06 21:40:46.330005 - C tpye: 0108521671-mrthanh@ubot.vn-T8- So hoa don tren TCT: 1
+2022-10-06 21:41:22.946078 - K tpye: 0108521671-mrthanh@ubot.vn-T8- So hoa don tren TCT: 10
+2022-10-06 21:41:34.088184 - C tpye: 0108521671-mrthanh@ubot.vn-T9- So hoa don tren TCT: 2
+2022-10-06 21:41:44.349730 - K tpye: 0108521671-mrthanh@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-06 21:43:01.072085 - C tpye: 0109869856-mrthanh@ubot.vn-T8- So hoa don tren TCT: 81
+2022-10-06 21:43:13.900884 - K tpye: 0109869856-mrthanh@ubot.vn-T8- So hoa don tren TCT: 3
+2022-10-06 21:44:15.212961 - C tpye: 0109869856-mrthanh@ubot.vn-T9- So hoa don tren TCT: 58
+2022-10-06 21:44:28.663046 - K tpye: 0109869856-mrthanh@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-06 21:56:13.514711 - C tpye: 0402155123-mrthanh@ubot.vn-T8- So hoa don tren TCT: 7
+2022-10-06 21:56:30.855203 - K tpye: 0402155123-mrthanh@ubot.vn-T8- So hoa don tren TCT: 6
+2022-10-06 21:56:37.959553 - C tpye: 0402155123-mrthanh@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-06 21:56:44.952420 - K tpye: 0402155123-mrthanh@ubot.vn-T9- No inv
+2022-10-06 21:57:21.169025 - C tpye: 3603854529-ngocvu@ubot.vn-T9- So hoa don tren TCT: 7
+2022-10-06 21:57:36.505400 - C tpye: 3603360865-ngocvu@ubot.vn-T9- So hoa don tren TCT: 8
+2022-10-06 21:57:58.019992 - C tpye: 3603258163-ngocvu@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-06 22:18:42.425429 - C tpye: 0302030402-ngocvu@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-06 22:18:55.783405 - C tpye: 3603796450-ngocvu@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-06 22:19:06.450988 - C tpye: 3603749450-ngocvu@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-06 22:19:53.487175 - C tpye: 3603272714-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 33
+2022-10-06 22:20:19.122869 - K tpye: 3603272714-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-06 22:20:57.118318 - C tpye: 0316956070-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 37
+2022-10-06 22:22:02.500198 - K tpye: 0316956070-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 51
+2022-10-06 22:22:07.586829 - C tpye: 0316137378-vohuyuy@ubot.vn-T9- No inv
+2022-10-06 22:24:30.783732 - K tpye: 0316137378-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 8
+2022-10-06 22:24:59.541614 - C tpye: 0315406109-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 13
+2022-10-06 22:25:06.349855 - K tpye: 0315406109-vohuyuy@ubot.vn-T9- No inv
+2022-10-06 22:25:18.542162 - C tpye: 0311789154-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 10
+2022-10-06 22:25:26.253442 - K tpye: 0311789154-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-06 22:25:32.359016 - C tpye: 0315567723-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-06 22:25:40.515856 - K tpye: 0315567723-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-06 22:26:13.109556 - C tpye: 0314480861-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-06 22:36:39.659301 - K tpye: 0314480861-vohuyuy@ubot.vn-T9- No inv
+2022-10-06 22:37:02.915465 - C tpye: 0317139441-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-06 22:37:19.305838 - K tpye: 0317139441-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-06 22:42:32.457776 - C tpye: 0316645170-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-06 22:43:56.795944 - K tpye: 0316645170-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 42
+2022-10-06 22:50:19.906835 - C tpye: 1900453371-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 32
+2022-10-06 22:50:57.064628 - K tpye: 1900453371-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-06 22:51:06.644612 - C tpye: 0315272938-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-06 22:51:20.754842 - K tpye: 0315272938-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-06 22:51:29.357249 - C tpye: 0315656966-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-06 22:52:00.947236 - K tpye: 0315656966-vohuyuy@ubot.vn-T9- No inv
+2022-10-06 22:52:34.657276 - C tpye: 0315046230-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 13
+2022-10-06 22:52:47.154568 - K tpye: 0315046230-vohuyuy@ubot.vn-T9- No inv
+2022-10-06 22:53:37.771277 - C tpye: 0401763496-ktdv43@ubot.vn-T9- So hoa don tren TCT: 46
+2022-10-07 04:56:23.305583 - C tpye: 0108466678-mrthanh@ubot.vn-T8- So hoa don tren TCT: 8
+2022-10-07 04:56:40.068660 - K tpye: 0108466678-mrthanh@ubot.vn-T8- So hoa don tren TCT: 23
+2022-10-07 04:57:07.940943 - C tpye: 0108466678-mrthanh@ubot.vn-T9- So hoa don tren TCT: 10
+2022-10-07 04:57:24.047383 - K tpye: 0108466678-mrthanh@ubot.vn-T9- So hoa don tren TCT: 13
+2022-10-07 04:57:28.896001 - C tpye: 0108543932-mrthanh@ubot.vn-T8- No inv
+2022-10-07 04:57:50.195017 - K tpye: 0108543932-mrthanh@ubot.vn-T8- So hoa don tren TCT: 11
+2022-10-07 04:57:57.996430 - C tpye: 0108543932-mrthanh@ubot.vn-T9- No inv
+2022-10-07 04:58:04.179524 - K tpye: 0108543932-mrthanh@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-07 04:58:15.413685 - C tpye: 0108521671-mrthanh@ubot.vn-T8- So hoa don tren TCT: 1
+2022-10-07 04:58:35.170122 - K tpye: 0108521671-mrthanh@ubot.vn-T8- So hoa don tren TCT: 10
+2022-10-07 04:58:48.832395 - C tpye: 0108521671-mrthanh@ubot.vn-T9- So hoa don tren TCT: 2
+2022-10-07 05:00:47.222334 - K tpye: 0108521671-mrthanh@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-07 05:01:46.302798 - C tpye: 0109869856-mrthanh@ubot.vn-T8- So hoa don tren TCT: 81
+2022-10-07 05:02:06.625576 - K tpye: 0109869856-mrthanh@ubot.vn-T8- So hoa don tren TCT: 3
+2022-10-07 05:02:59.438749 - C tpye: 0109869856-mrthanh@ubot.vn-T9- So hoa don tren TCT: 58
+2022-10-07 05:03:08.795781 - K tpye: 0109869856-mrthanh@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-07 05:03:18.657383 - C tpye: 0402155123-mrthanh@ubot.vn-T8- So hoa don tren TCT: 7
+2022-10-07 05:03:41.642842 - K tpye: 0402155123-mrthanh@ubot.vn-T8- So hoa don tren TCT: 6
+2022-10-07 05:03:49.830875 - C tpye: 0402155123-mrthanh@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-07 05:03:58.340992 - K tpye: 0402155123-mrthanh@ubot.vn-T9- No inv
+2022-10-07 05:04:08.643828 - C tpye: 3603854529-ngocvu@ubot.vn-T9- So hoa don tren TCT: 7
+2022-10-07 05:04:42.355037 - C tpye: 3603360865-ngocvu@ubot.vn-T9- So hoa don tren TCT: 8
+2022-10-07 05:06:02.979656 - C tpye: 3603258163-ngocvu@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-07 05:11:24.057230 - C tpye: 0302030402-ngocvu@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-07 05:11:37.397461 - C tpye: 3603796450-ngocvu@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-07 05:11:52.878991 - C tpye: 3603749450-ngocvu@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-07 05:12:46.280060 - C tpye: 3603272714-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 33
+2022-10-07 05:13:04.595521 - K tpye: 3603272714-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-07 05:13:34.218339 - C tpye: 0316956070-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 37
+2022-10-07 05:40:29.741287 - K tpye: 0316956070-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 51
+2022-10-07 05:42:09.254625 - C tpye: 0316137378-vohuyuy@ubot.vn-T9- No inv
+2022-10-07 05:43:00.620328 - K tpye: 0316137378-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 8
+2022-10-07 05:44:04.393823 - C tpye: 0315406109-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 13
+2022-10-07 05:44:24.260176 - K tpye: 0315406109-vohuyuy@ubot.vn-T9- No inv
+2022-10-07 05:44:48.670447 - C tpye: 0311789154-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 10
+2022-10-07 05:44:57.147340 - K tpye: 0311789154-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-07 05:45:04.470757 - C tpye: 0315567723-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-07 05:45:21.010209 - K tpye: 0315567723-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-07 05:45:36.781613 - C tpye: 0314480861-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-07 05:45:41.560826 - K tpye: 0314480861-vohuyuy@ubot.vn-T9- No inv
+2022-10-07 05:45:53.515597 - C tpye: 0317139441-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-07 05:46:01.670989 - K tpye: 0317139441-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-07 05:46:10.219255 - C tpye: 0316645170-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-07 05:47:10.073554 - K tpye: 0316645170-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 42
+2022-10-07 05:47:44.769372 - C tpye: 1900453371-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 32
+2022-10-07 05:49:24.186639 - K tpye: 1900453371-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-07 05:54:47.528361 - C tpye: 0315272938-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-07 05:55:33.550928 - K tpye: 0315272938-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-07 05:55:56.655896 - C tpye: 0315656966-vohuyuy@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-07 05:56:04.450480 - K tpye: 0315656966-vohuyuy@ubot.vn-T9- No inv
+2022-10-07 05:59:26.193498 - C tpye: 0401763496-ktdv43@ubot.vn-T9- So hoa don tren TCT: 46
+2022-10-07 06:00:57.265235 - C tpye: 0401963199-ktdv43@ubot.vn-T9- So hoa don tren TCT: 76
+2022-10-07 06:01:08.930791 - C tpye: 0402143110-ktdv43@ubot.vn-T9- DANG NHAP THAT BAI: SAI MAT KHAU
+2022-10-07 06:01:42.652005 - C tpye: 0402095682-ktdv43@ubot.vn-T9- So hoa don tren TCT: 8
+2022-10-07 06:02:28.282920 - C tpye: 0401535884-ktdv43@ubot.vn-T9- So hoa don tren TCT: 23
+2022-10-07 06:02:45.626709 - C tpye: 0314146585-ktdv43@ubot.vn-T9- So hoa don tren TCT: 6
+2022-10-07 06:04:51.156934 - C tpye: 0401824935-ktdv43@ubot.vn-T9- So hoa don tren TCT: 14
+2022-10-07 06:05:02.249146 - C tpye: 0110068554-thuy212@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-07 06:05:28.356620 - C tpye: 0109279913-thuy212@ubot.vn-T7- So hoa don tren TCT: 3
+2022-10-07 06:05:47.607202 - C tpye: 0109279913-thuy212@ubot.vn-T8- So hoa don tren TCT: 3
+2022-10-07 06:11:26.424673 - C tpye: 0109279913-thuy212@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-07 06:16:44.550634 - C tpye: 0109149061-thuy212@ubot.vn-T7- So hoa don tren TCT: 1
+2022-10-07 06:17:07.521262 - C tpye: 0109149061-thuy212@ubot.vn-T8- So hoa don tren TCT: 2
+2022-10-07 06:22:33.604869 - C tpye: 0109149061-thuy212@ubot.vn-T9- So hoa don tren TCT: 2
+2022-10-07 06:23:04.008815 - C tpye: 0317196753-wemi@ubot.vn-T9- So hoa don tren TCT: 30
+2022-10-07 06:24:52.327231 - C tpye: 0801176189-thucphamvt@ubot.vn-T9- So hoa don tren TCT: 105
+2022-10-07 06:30:45.647831 - C tpye: 0700218932-ketoanaccs@ubot.vn-T9- So hoa don tren TCT: 25
+2022-10-07 06:31:19.903200 - C tpye: 0700858523-ketoanaccs@ubot.vn-T9- So hoa don tren TCT: 29
+2022-10-07 06:32:21.523724 - C tpye: 0700789862-ketoanaccs@ubot.vn-T9- So hoa don tren TCT: 26
+2022-10-07 06:32:53.839274 - C tpye: 0316600927-dj@ubot.vn-T1- So hoa don tren TCT: 24
+2022-10-07 06:37:49.708797 - C tpye: 0316600927-dj@ubot.vn-T9- So hoa don tren TCT: 4
+2022-10-07 06:38:04.442545 - C tpye: 110079919-dzlbros@ubot.vn-T9- DANG NHAP THAT BAI: SAI MAT KHAU
+2022-10-07 06:38:20.968405 - C tpye: 109184299-dzlbros@ubot.vn-T9- DANG NHAP THAT BAI: SAI MAT KHAU
+2022-10-07 06:38:27.154729 - C tpye: 107550221-dzlbros@ubot.vn-T9- DANG NHAP THAT BAI: SAI MAT KHAU
+2022-10-07 06:38:34.653887 - C tpye: 109199898-dzlbros@ubot.vn-T9- DANG NHAP THAT BAI: SAI MAT KHAU
+2022-10-07 06:38:40.548683 - C tpye: 2500553373-dzlbros@ubot.vn-T9- So hoa don tren TCT: 3
+2022-10-07 06:38:53.686991 - C tpye: 1001239258-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 10
+2022-10-07 06:39:12.622456 - K tpye: 1001239258-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 6
+2022-10-07 06:39:43.290676 - C tpye: 1001239258-phanhuyen@ubot.vn-T9- So hoa don tren TCT: 16
+2022-10-07 06:39:55.100393 - K tpye: 1001239258-phanhuyen@ubot.vn-T9- So hoa don tren TCT: 1
+2022-10-07 06:40:11.275809 - C tpye: 0202101385-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 3
+2022-10-07 06:40:41.106735 - K tpye: 0202101385-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 2
+2022-10-07 06:41:06.596519 - C tpye: 0202101385-phanhuyen@ubot.vn-T9- So hoa don tren TCT: 2
+2022-10-07 06:41:15.952901 - K tpye: 0202101385-phanhuyen@ubot.vn-T9- No inv
+2022-10-07 06:41:24.546825 - C tpye: 0202004303-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 2
+2022-10-07 06:41:33.666083 - K tpye: 0202004303-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 1
+2022-10-07 06:43:16.295396 - C tpye: 0202004303-phanhuyen@ubot.vn-T9- So hoa don tren TCT: 5
+2022-10-07 06:43:51.568849 - K tpye: 0202004303-phanhuyen@ubot.vn-T9- No inv
+2022-10-07 06:44:50.348551 - C tpye: 0202099707-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 57
+2022-10-07 06:45:57.253646 - K tpye: 0202099707-phanhuyen@ubot.vn-T8- So hoa don tren TCT: 58
+2022-10-07 06:47:53.930048 - C tpye: 0202099707-phanhuyen@ubot.vn-T9- So hoa don tren TCT: 74
+2022-10-07 06:49:28.853993 - K tpye: 0202099707-phanhuyen@ubot.vn-T9- So hoa don tren TCT: 75
+2022-10-07 06:49:34.010736 - C tpye: 2500580137-domovp@ubot.vn-T8- DANG NHAP THAT BAI: TK BI KHOA
+2022-10-07 06:49:39.694790 - K tpye: 2500580137-domovp@ubot.vn-T8- DANG NHAP THAT BAI: TK BI KHOA
+2022-10-07 06:49:44.421423 - C tpye: 2500580137-domovp@ubot.vn-T9- DANG NHAP THAT BAI: TK BI KHOA
+2022-10-07 06:50:19.229252 - K tpye: 2500580137-domovp@ubot.vn-T9- DANG NHAP THAT BAI: TK BI KHOA
+2022-10-07 06:51:21.850921 - C tpye: 2500508620-domovp@ubot.vn-T8- So hoa don tren TCT: 25
+2022-10-07 06:52:53.834130 - K tpye: 2500508620-domovp@ubot.vn-T8- So hoa don tren TCT: 33
+2022-10-07 06:54:13.283383 - C tpye: 2500508620-domovp@ubot.vn-T9- So hoa don tren TCT: 106
+2022-10-07 06:54:35.080251 - K tpye: 2500508620-domovp@ubot.vn-T9- So hoa don tren TCT: 7
+2022-10-07 06:55:04.621284 - C tpye: 2500592478-domovp@ubot.vn-T8- So hoa don tren TCT: 7
+2022-10-07 07:00:24.786940 - K tpye: 2500592478-domovp@ubot.vn-T8- So hoa don tren TCT: 4
+2022-10-07 07:00:45.425288 - C tpye: 2500592478-domovp@ubot.vn-T9- So hoa don tren TCT: 19
+2022-10-07 07:00:51.509659 - K tpye: 2500592478-domovp@ubot.vn-T9- No inv
