e5646fac1b6d4223e6ff877c2e3bfb2df2874a76
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2022-11-06 18:22:36 +0700

    Create 20221104.cpp

---
+++ T11/20221104.cpp
@@ -0,0 +1,119 @@
+
+
+2022-11-04 10:01:40.342994 - C tpye: 3702791809-chungngoc@ubot.vn-T10- So hoa don tren TCT: 1
+2022-11-04 10:01:46.537501 - C tpye: 3702955729-chungngoc@ubot.vn-T10- So hoa don tren TCT: 1
+2022-11-04 10:01:51.574249 - C tpye: 1702135666-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:02:12.600409 - C tpye: 0315149317-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:02:33.305885 - C tpye: 3702888670-chungngoc@ubot.vn-T10- So hoa don tren TCT: 2
+2022-11-04 10:02:41.382064 - C tpye: 3801200045-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:03:09.973850 - C tpye: 3702960990-chungngoc@ubot.vn-T10- So hoa don tren TCT: 10
+2022-11-04 10:03:39.197969 - C tpye: 3702769070-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:03:46.533973 - C tpye: 3702794574-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:03:53.817155 - C tpye: 3702880689-chungngoc@ubot.vn-T10- DANG NHAP THAT BAI: {"TIMESTAMP":"04/11/2022 10:03:51","MESSAGE":"JSON PARSE ERROR: INVALID UTF-8 MIDDLE BYTE 0X6E; NESTED EXCEPTION IS COM.FASTERXML.JACKSON.DATABIND.JSONMAPPINGEXCEPTION: INVALID UTF-8 MIDDLE BYTE 0X6E\N AT [SOURCE: (PUSHBACKINPUTSTREAM); LINE: 1, COLUMN: 42] (THROUGH REFERENCE CHAIN: VN.TECA.HDDT.SECURITY.MODEL.JWTREQUEST[\"PASSWORD\"])","DETAILS":"","PATH":"URI=/AUTHENTICATE"}
+2022-11-04 10:04:37.415513 - C tpye: 3702866074-chungngoc@ubot.vn-T10- So hoa don tren TCT: 33
+2022-11-04 10:04:45.131066 - C tpye: 3801094301-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:05:11.519744 - C tpye: 3702820584-chungngoc@ubot.vn-T10- So hoa don tren TCT: 12
+2022-11-04 10:05:17.472294 - C tpye: 3702990459-chungngoc@ubot.vn-T10- So hoa don tren TCT: 5
+2022-11-04 10:05:25.437930 - C tpye: 0402087699-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:05:56.030206 - C tpye: 3702935225-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:06:05.528448 - C tpye: 3702395724-chungngoc@ubot.vn-T10- DANG NHAP THAT BAI: {"TIMESTAMP":"04/11/2022 10:06:03","MESSAGE":"JSON PARSE ERROR: INVALID UTF-8 MIDDLE BYTE 0X6E; NESTED EXCEPTION IS COM.FASTERXML.JACKSON.DATABIND.JSONMAPPINGEXCEPTION: INVALID UTF-8 MIDDLE BYTE 0X6E\N AT [SOURCE: (PUSHBACKINPUTSTREAM); LINE: 1, COLUMN: 42] (THROUGH REFERENCE CHAIN: VN.TECA.HDDT.SECURITY.MODEL.JWTREQUEST[\"PASSWORD\"])","DETAILS":"","PATH":"URI=/AUTHENTICATE"}
+2022-11-04 10:06:12.734155 - C tpye: 3801168200-chungngoc@ubot.vn-T10- DANG NHAP THAT BAI: SAI MAT KHAU
+2022-11-04 10:06:20.539033 - C tpye: 3701137287-chungngoc@ubot.vn-T10- DANG NHAP THAT BAI: {"TIMESTAMP":"04/11/2022 10:06:18","MESSAGE":"JSON PARSE ERROR: INVALID UTF-8 MIDDLE BYTE 0X6E; NESTED EXCEPTION IS COM.FASTERXML.JACKSON.DATABIND.JSONMAPPINGEXCEPTION: INVALID UTF-8 MIDDLE BYTE 0X6E\N AT [SOURCE: (PUSHBACKINPUTSTREAM); LINE: 1, COLUMN: 42] (THROUGH REFERENCE CHAIN: VN.TECA.HDDT.SECURITY.MODEL.JWTREQUEST[\"PASSWORD\"])","DETAILS":"","PATH":"URI=/AUTHENTICATE"}
+2022-11-04 10:06:44.137464 - C tpye: 3702962814-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:06:53.118307 - C tpye: 3702827205-chungngoc@ubot.vn-T10- DANG NHAP THAT BAI: TK BI KHOA
+2022-11-04 10:07:07.490179 - C tpye: 3702955380-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:07:52.694325 - C tpye: 3801270571-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:08:00.834165 - C tpye: 3702785315-chungngoc@ubot.vn-T10- So hoa don tren TCT: 2
+2022-11-04 10:08:21.539663 - C tpye: 3901283454-chungngoc@ubot.vn-T10- So hoa don tren TCT: 3
+2022-11-04 10:08:38.949297 - C tpye: 3703078693-chungngoc@ubot.vn-T10- So hoa don tren TCT: 1
+2022-11-04 10:08:46.839739 - C tpye: 3702528639-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:08:54.913385 - C tpye: 3700364872-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:09:12.244632 - C tpye: 3801168200-chungngoc@ubot.vn-T10- No inv
+2022-11-04 10:09:46.931657 - C tpye: 0107125675-mkb@ubot.vn-T10- So hoa don tren TCT: 7
+2022-11-04 10:10:01.895283 - C tpye: 2801290713-tranthiphuong@ubot.vn-T10- So hoa don tren TCT: 7
+2022-11-04 10:10:08.023728 - K tpye: 2801290713-tranthiphuong@ubot.vn-T10- No inv
+2022-11-04 10:10:44.722692 - C tpye: 2801278610-tranthiphuong@ubot.vn-T10- So hoa don tren TCT: 14
+2022-11-04 10:10:57.147806 - K tpye: 2801278610-tranthiphuong@ubot.vn-T10- So hoa don tren TCT: 8
+2022-11-04 10:13:59.259167 - C tpye: 3701793200-kimcuong@ubot.vn-T10- So hoa don tren TCT: 149
+2022-11-04 10:14:56.673045 - K tpye: 3701793200-kimcuong@ubot.vn-T10- So hoa don tren TCT: 64
+2022-11-04 10:15:05.734110 - C tpye: 0700847585-Khietpham@ubot.vn-T10- No inv
+2022-11-04 10:16:08.269533 - C tpye: 0309759441-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 69
+2022-11-04 10:16:15.078000 - K tpye: 0309759441-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 9
+2022-11-04 10:16:23.770226 - C tpye: 0309759441-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 3
+2022-11-04 10:16:54.598306 - K tpye: 0309759441-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 1
+2022-11-04 10:17:39.862032 - C tpye: 0315878736-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 45
+2022-11-04 10:18:20.388181 - K tpye: 0315878736-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 37
+2022-11-04 10:18:35.041018 - C tpye: 0315878736-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 5
+2022-11-04 10:18:45.931667 - K tpye: 0315878736-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 6
+2022-11-04 10:19:40.054741 - C tpye: 0315666467-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 38
+2022-11-04 10:19:52.462607 - K tpye: 0315666467-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 5
+2022-11-04 10:20:07.647875 - C tpye: 0315666467-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 2
+2022-11-04 10:20:15.994041 - K tpye: 0315666467-thanhcongact@ubot.vn-T11- No inv
+2022-11-04 10:21:56.474059 - C tpye: 0312988988-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 96
+2022-11-04 10:23:07.015012 - K tpye: 0312988988-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 18
+2022-11-04 10:23:25.071980 - C tpye: 0312988988-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 8
+2022-11-04 10:23:36.943605 - K tpye: 0312988988-thanhcongact@ubot.vn-T11- No inv
+2022-11-04 10:24:30.803138 - C tpye: 0313217723-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 58
+2022-11-04 10:24:58.084575 - K tpye: 0313217723-thanhcongact@ubot.vn-T10- So hoa don tren TCT: 22
+2022-11-04 10:25:09.705257 - C tpye: 0313217723-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 9
+2022-11-04 10:25:19.013577 - K tpye: 0313217723-thanhcongact@ubot.vn-T11- So hoa don tren TCT: 1
+2022-11-04 10:25:23.041474 - C tpye: 0100774631-021-thanhcongact@ubot.vn-T10- DANG NHAP THAT BAI: SAI MAT KHAU
+2022-11-04 10:25:28.827294 - K tpye: 0100774631-021-thanhcongact@ubot.vn-T10- DANG NHAP THAT BAI: TK BI KHOA
+2022-11-04 10:25:34.251115 - C tpye: 0100774631-021-thanhcongact@ubot.vn-T11- DANG NHAP THAT BAI: TK BI KHOA
+2022-11-04 10:25:38.614342 - K tpye: 0100774631-021-thanhcongact@ubot.vn-T11- DANG NHAP THAT BAI: TK BI KHOA
+2022-11-04 10:25:46.685844 - C tpye: 0601194945-duongoanh@ubot.vn-T1- No inv
+2022-11-04 10:25:55.347706 - K tpye: 0601194945-duongoanh@ubot.vn-T1- No inv
+2022-11-04 10:26:03.151130 - C tpye: 0601194945-duongoanh@ubot.vn-T2- No inv
+2022-11-04 10:26:13.554434 - K tpye: 0601194945-duongoanh@ubot.vn-T2- No inv
+2022-11-04 10:26:27.891682 - C tpye: 0601194945-duongoanh@ubot.vn-T3- So hoa don tren TCT: 2
+2022-11-04 10:26:34.054994 - K tpye: 0601194945-duongoanh@ubot.vn-T3- No inv
+2022-11-04 10:26:43.376964 - C tpye: 0601194945-duongoanh@ubot.vn-T4- So hoa don tren TCT: 1
+2022-11-04 10:26:54.532380 - K tpye: 0601194945-duongoanh@ubot.vn-T4- So hoa don tren TCT: 4
+2022-11-04 10:27:02.439499 - C tpye: 0601194945-duongoanh@ubot.vn-T5- So hoa don tren TCT: 1
+2022-11-04 10:27:10.552736 - K tpye: 0601194945-duongoanh@ubot.vn-T5- So hoa don tren TCT: 1
+2022-11-04 10:27:28.534061 - C tpye: 0601194945-duongoanh@ubot.vn-T6- So hoa don tren TCT: 2
+2022-11-04 10:27:37.364679 - K tpye: 0601194945-duongoanh@ubot.vn-T6- So hoa don tren TCT: 3
+2022-11-04 10:27:44.955596 - C tpye: 0601194945-duongoanh@ubot.vn-T7- So hoa don tren TCT: 2
+2022-11-04 10:27:57.323428 - K tpye: 0601194945-duongoanh@ubot.vn-T7- So hoa don tren TCT: 7
+2022-11-04 10:28:07.527942 - C tpye: 0601194945-duongoanh@ubot.vn-T8- So hoa don tren TCT: 1
+2022-11-04 10:28:18.657368 - K tpye: 0601194945-duongoanh@ubot.vn-T8- So hoa don tren TCT: 8
+2022-11-04 10:28:29.816956 - C tpye: 0601194945-duongoanh@ubot.vn-T9- So hoa don tren TCT: 4
+2022-11-04 10:28:39.085613 - K tpye: 0601194945-duongoanh@ubot.vn-T9- So hoa don tren TCT: 4
+2022-11-04 10:29:02.841495 - C tpye: 0601194945-duongoanh@ubot.vn-T10- No inv
+2022-11-04 10:29:10.328630 - K tpye: 0601194945-duongoanh@ubot.vn-T10- So hoa don tren TCT: 6
+2022-11-04 10:29:17.078765 - C tpye: 0600333995-duongoanh@ubot.vn-T1- No inv
+2022-11-04 10:29:37.294339 - K tpye: 0600333995-duongoanh@ubot.vn-T1- No inv
+2022-11-04 10:29:43.850323 - C tpye: 0600333995-duongoanh@ubot.vn-T2- No inv
+2022-11-04 10:29:51.409533 - K tpye: 0600333995-duongoanh@ubot.vn-T2- No inv
+2022-11-04 10:29:58.011177 - C tpye: 0600333995-duongoanh@ubot.vn-T3- No inv
+2022-11-04 10:30:04.961875 - K tpye: 0600333995-duongoanh@ubot.vn-T3- No inv
+2022-11-04 10:30:16.588799 - C tpye: 0600333995-duongoanh@ubot.vn-T4- No inv
+2022-11-04 10:31:18.842051 - K tpye: 0600333995-duongoanh@ubot.vn-T4- So hoa don tren TCT: 68
+2022-11-04 10:31:28.500441 - C tpye: 0600333995-duongoanh@ubot.vn-T5- No inv
+2022-11-04 10:32:28.006720 - K tpye: 0600333995-duongoanh@ubot.vn-T5- So hoa don tren TCT: 48
+2022-11-04 10:32:33.870763 - C tpye: 0600333995-duongoanh@ubot.vn-T6- No inv
+2022-11-04 10:33:08.650236 - K tpye: 0600333995-duongoanh@ubot.vn-T6- So hoa don tren TCT: 43
+2022-11-04 10:33:15.907811 - C tpye: 0600333995-duongoanh@ubot.vn-T7- So hoa don tren TCT: 1
+2022-11-04 10:34:38.863883 - K tpye: 0600333995-duongoanh@ubot.vn-T7- So hoa don tren TCT: 49
+2022-11-04 10:35:02.670036 - C tpye: 0600333995-duongoanh@ubot.vn-T8- So hoa don tren TCT: 21
+2022-11-04 10:35:59.611347 - K tpye: 0600333995-duongoanh@ubot.vn-T8- So hoa don tren TCT: 57
+2022-11-04 10:36:13.139925 - C tpye: 0600333995-duongoanh@ubot.vn-T9- So hoa don tren TCT: 3
+2022-11-04 10:37:09.095876 - K tpye: 0600333995-duongoanh@ubot.vn-T9- So hoa don tren TCT: 57
+2022-11-04 10:37:49.055919 - C tpye: 0600333995-duongoanh@ubot.vn-T10- So hoa don tren TCT: 21
+2022-11-04 10:37:57.484207 - K tpye: 0600333995-duongoanh@ubot.vn-T10- So hoa don tren TCT: 5
+2022-11-04 13:31:55.306674 - SUCCESS - OUTPUT: 0309759441-T11- Excel co 4 hoa don, tai ve duoc 4 hoa don
+2022-11-04 13:33:20.743019 - SUCCESS - OUTPUT: 0315878736-T11- Excel co 18 hoa don, tai ve duoc 18 hoa don
+2022-11-04 13:33:27.597074 - ERROR - OUTPUT: 0315666467-T11- No inv
+2022-11-04 13:34:26.065133 - SUCCESS - OUTPUT: 0312988988-T11- Excel co 38 hoa don, tai ve duoc 38 hoa don
+2022-11-04 13:36:18.389552 - SUCCESS - OUTPUT: 0309759441-T11- Excel co 4 hoa don, tai ve duoc 4 hoa don
+2022-11-04 13:36:51.498109 - SUCCESS - OUTPUT: 0315878736-T11- Excel co 18 hoa don, tai ve duoc 18 hoa don
+2022-11-04 13:37:13.113794 - ERROR - OUTPUT: 0315666467-T11- No inv
+2022-11-04 13:43:14.245282 - SUCCESS - OUTPUT: 0313217723-T11- Excel co 16 hoa don, tai ve duoc 16 hoa don
+2022-11-04 13:48:52.384386 - SUCCESS - OUTPUT: 0312988988-T11- Excel co 38 hoa don, tai ve duoc 38 hoa don
+2022-11-04 13:53:38.110904 - SUCCESS - OUTPUT: 3701697779-T11- Excel co 7 hoa don, tai ve duoc 7 hoa don
+2022-11-04 13:54:47.984193 - SUCCESS - OUTPUT: 0309759441-T10- Excel co 23 hoa don, tai ve duoc 23 hoa don
+2022-11-04 13:57:13.382611 - SUCCESS - OUTPUT: 0315878736-T10- Excel co 109 hoa don, tai ve duoc 109 hoa don
+2022-11-04 13:57:45.670179 - SUCCESS - OUTPUT: 0315666467-T10- Excel co 8 hoa don, tai ve duoc 8 hoa don
+2022-11-04 14:00:23.921644 - SUCCESS - OUTPUT: 0312988988-T10- Excel co 93 hoa don, tai ve duoc 93 hoa don
+2022-11-04 14:08:07.040774 - SUCCESS - OUTPUT: 0313217723-T10- Excel co 112 hoa don, tai ve duoc 112 hoa don
+2022-11-04 14:13:31.167661 - SUCCESS - OUTPUT: 3701697779-T10- Excel co 225 hoa don, tai ve duoc 225 hoa don
