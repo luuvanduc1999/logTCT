e8e0fdb7d83ef7056ab53766c987f04bb253a485
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2022-11-30 09:03:58 +0700

    Create 20221129.cpp

---
+++ T11/20221129.cpp
@@ -0,0 +1,48 @@
+
+2022-11-29 22:33:39.813261 - C tpye: 0102042513-afg@ubot.vn-T11- So hoa don tren TCT: 534
+2022-11-29 22:53:40.714278 - K tpye: 0102042513-afg@ubot.vn-T11- So hoa don tren TCT: 309
+2022-11-29 23:36:34.354274 - C tpye: 0100233368-afg@ubot.vn-T11- So hoa don tren TCT: 712
+2022-11-30 00:13:01.991179 - K tpye: 0100233368-afg@ubot.vn-T11- So hoa don tren TCT: 564
+2022-11-30 00:13:11.748884 - C tpye: 0313344030-laithity1973@ubot.vn-T1- No inv
+2022-11-30 00:13:48.043114 - K tpye: 0313344030-laithity1973@ubot.vn-T1- No inv
+2022-11-30 00:14:27.808963 - C tpye: 0313344030-laithity1973@ubot.vn-T2- No inv
+2022-11-30 00:15:03.290323 - K tpye: 0313344030-laithity1973@ubot.vn-T2- No inv
+2022-11-30 00:16:09.749671 - C tpye: 0313344030-laithity1973@ubot.vn-T3- So hoa don tren TCT: 1
+2022-11-30 00:16:27.875595 - K tpye: 0313344030-laithity1973@ubot.vn-T3- No inv
+2022-11-30 00:16:43.244464 - C tpye: 0313344030-laithity1973@ubot.vn-T4- So hoa don tren TCT: 4
+2022-11-30 00:17:05.772701 - K tpye: 0313344030-laithity1973@ubot.vn-T4- So hoa don tren TCT: 1
+2022-11-30 00:17:36.483853 - C tpye: 0313344030-laithity1973@ubot.vn-T5- So hoa don tren TCT: 4
+2022-11-30 00:17:43.733251 - K tpye: 0313344030-laithity1973@ubot.vn-T5- So hoa don tren TCT: 1
+2022-11-30 00:18:17.536585 - C tpye: 0313344030-laithity1973@ubot.vn-T6- So hoa don tren TCT: 4
+2022-11-30 00:18:40.581062 - K tpye: 0313344030-laithity1973@ubot.vn-T6- So hoa don tren TCT: 1
+2022-11-30 00:18:59.522633 - C tpye: 0313344030-laithity1973@ubot.vn-T7- So hoa don tren TCT: 5
+2022-11-30 00:19:38.473155 - K tpye: 0313344030-laithity1973@ubot.vn-T7- So hoa don tren TCT: 2
+2022-11-30 00:19:45.333804 - C tpye: 0313344030-laithity1973@ubot.vn-T8- So hoa don tren TCT: 2
+2022-11-30 00:19:53.941153 - K tpye: 0313344030-laithity1973@ubot.vn-T8- So hoa don tren TCT: 2
+2022-11-30 00:20:15.227501 - C tpye: 0313344030-laithity1973@ubot.vn-T9- So hoa don tren TCT: 5
+2022-11-30 00:20:46.469442 - K tpye: 0313344030-laithity1973@ubot.vn-T9- So hoa don tren TCT: 3
+2022-11-30 00:21:17.658789 - C tpye: 0313344030-laithity1973@ubot.vn-T10- So hoa don tren TCT: 4
+2022-11-30 00:21:24.575592 - K tpye: 0313344030-laithity1973@ubot.vn-T10- So hoa don tren TCT: 3
+2022-11-30 00:21:33.781000 - C tpye: 0313344030-laithity1973@ubot.vn-T11- So hoa don tren TCT: 2
+2022-11-30 00:21:42.438553 - K tpye: 0313344030-laithity1973@ubot.vn-T11- So hoa don tren TCT: 1
+2022-11-30 00:21:51.456481 - C tpye: 0316948591-greenwise@ubot.vn-T7- No inv
+2022-11-30 00:21:59.019314 - K tpye: 0316948591-greenwise@ubot.vn-T7- No inv
+2022-11-30 00:22:04.675628 - C tpye: 0316948591-greenwise@ubot.vn-T8- So hoa don tren TCT: 2
+2022-11-30 00:22:12.440416 - K tpye: 0316948591-greenwise@ubot.vn-T8- No inv
+2022-11-30 00:22:33.008360 - C tpye: 0316948591-greenwise@ubot.vn-T9- So hoa don tren TCT: 1
+2022-11-30 00:22:53.226808 - K tpye: 0316948591-greenwise@ubot.vn-T9- So hoa don tren TCT: 1
+2022-11-30 00:23:01.249802 - C tpye: 0316948591-greenwise@ubot.vn-T10- So hoa don tren TCT: 2
+2022-11-30 00:23:26.103445 - K tpye: 0316948591-greenwise@ubot.vn-T10- So hoa don tren TCT: 1
+2022-11-30 00:24:19.170392 - C tpye: 0316948591-greenwise@ubot.vn-T11- So hoa don tren TCT: 9
+2022-11-30 00:24:27.996567 - K tpye: 0316948591-greenwise@ubot.vn-T11- So hoa don tren TCT: 1
+2022-11-30 01:39:33.916286 - C tpye: 0313344030-laithity1973@ubot.vn-T1- No inv
+2022-11-30 01:40:03.505295 - C tpye: 0313344030-laithity1973@ubot.vn-T2- No inv
+2022-11-30 01:40:11.455294 - C tpye: 0313344030-laithity1973@ubot.vn-T3- So hoa don tren TCT: 1
+2022-11-30 01:40:58.115486 - C tpye: 0313344030-laithity1973@ubot.vn-T4- So hoa don tren TCT: 4
+2022-11-30 01:41:15.332436 - C tpye: 0313344030-laithity1973@ubot.vn-T5- So hoa don tren TCT: 4
+2022-11-30 01:41:37.396131 - C tpye: 0313344030-laithity1973@ubot.vn-T6- So hoa don tren TCT: 4
+2022-11-30 01:41:47.295432 - C tpye: 0313344030-laithity1973@ubot.vn-T7- So hoa don tren TCT: 5
+2022-11-30 01:41:57.959653 - C tpye: 0313344030-laithity1973@ubot.vn-T8- So hoa don tren TCT: 2
+2022-11-30 01:42:28.327921 - C tpye: 0313344030-laithity1973@ubot.vn-T9- So hoa don tren TCT: 5
+2022-11-30 01:42:51.243260 - C tpye: 0313344030-laithity1973@ubot.vn-T10- So hoa don tren TCT: 4
+2022-11-30 01:43:14.062168 - C tpye: 0313344030-laithity1973@ubot.vn-T11- So hoa don tren TCT: 2
