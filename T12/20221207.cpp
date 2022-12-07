d1fc753c30dec867500ef993cd8e15040f60d143
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2022-12-07 08:25:47 +0700

    Create 20221207.cpp

---
+++ T12/20221207.cpp
@@ -0,0 +1,4 @@
+2022-12-06 15:37:30.814960 - C tpye: 0308293824-xuanphuong@ubot.vn-T11- So hoa don tren TCT: 26
+2022-12-06 18:57:45.482476 - C tpye: 0308293824-xuanphuong@ubot.vn-T11- So hoa don tren TCT: 26
+2022-12-06 20:16:41.254139 - C tpye: 0316700223-ecovietnam@ubot.vn-T12- So hoa don tren TCT: 1
+2022-12-06 20:16:50.639384 - C tpye: 0108224622-ecohanoi@ubot.vn-T12- No inv
