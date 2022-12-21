b181b21c1e1a3aff733a3e6e54ffa225b1fa2a00
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2022-12-21 09:12:14 +0700

    Create 20221220.cpp

---
+++ T12/20221220.cpp
@@ -0,0 +1,51 @@
+
+2022-12-20 23:10:48.848643 - C tpye: 0313687567-p316@ubot.vn-T1- So hoa don tren TCT: 12
+2022-12-20 23:10:58.637344 - C tpye: 0313687567-p316@ubot.vn-T2- So hoa don tren TCT: 5
+2022-12-20 23:11:04.683619 - C tpye: 0313687567-p316@ubot.vn-T3- So hoa don tren TCT: 10
+2022-12-20 23:11:45.664703 - C tpye: 0313687567-p316@ubot.vn-T4- So hoa don tren TCT: 19
+2022-12-20 23:12:24.393741 - C tpye: 0313687567-p316@ubot.vn-T5- So hoa don tren TCT: 23
+2022-12-20 23:13:04.409101 - C tpye: 0313687567-p316@ubot.vn-T6- So hoa don tren TCT: 19
+2022-12-20 23:15:19.613864 - C tpye: 3702570091-hoangtu@ubot.vn-T7- So hoa don tren TCT: 87
+2022-12-20 23:17:12.860866 - K tpye: 3702570091-hoangtu@ubot.vn-T7- So hoa don tren TCT: 65
+2022-12-20 23:19:13.184358 - C tpye: 3702570091-hoangtu@ubot.vn-T8- So hoa don tren TCT: 88
+2022-12-20 23:22:06.316921 - K tpye: 3702570091-hoangtu@ubot.vn-T8- So hoa don tren TCT: 52
+2022-12-20 23:24:57.680254 - C tpye: 3702570091-hoangtu@ubot.vn-T9- So hoa don tren TCT: 95
+2022-12-20 23:27:21.393726 - K tpye: 3702570091-hoangtu@ubot.vn-T9- So hoa don tren TCT: 63
+2022-12-20 23:30:49.324130 - C tpye: 3702570091-hoangtu@ubot.vn-T10- So hoa don tren TCT: 107
+2022-12-20 23:33:43.762938 - K tpye: 3702570091-hoangtu@ubot.vn-T10- So hoa don tren TCT: 76
+2022-12-20 23:36:45.573990 - C tpye: 3702570091-hoangtu@ubot.vn-T11- So hoa don tren TCT: 124
+2022-12-20 23:38:55.894963 - K tpye: 3702570091-hoangtu@ubot.vn-T11- So hoa don tren TCT: 69
+2022-12-20 23:41:00.201607 - C tpye: 3702570091-hoangtu@ubot.vn-T12- So hoa don tren TCT: 66
+2022-12-20 23:41:47.797160 - K tpye: 3702570091-hoangtu@ubot.vn-T12- So hoa don tren TCT: 16
+2022-12-20 23:44:14.486800 - C tpye: 3702355150-hoangtu@ubot.vn-T7- So hoa don tren TCT: 72
+2022-12-20 23:45:40.355338 - K tpye: 3702355150-hoangtu@ubot.vn-T7- So hoa don tren TCT: 51
+2022-12-20 23:47:51.625755 - C tpye: 3702355150-hoangtu@ubot.vn-T8- So hoa don tren TCT: 86
+2022-12-20 23:50:26.311343 - K tpye: 3702355150-hoangtu@ubot.vn-T8- So hoa don tren TCT: 52
+2022-12-20 23:52:13.974930 - C tpye: 3702355150-hoangtu@ubot.vn-T9- So hoa don tren TCT: 62
+2022-12-20 23:53:34.516031 - K tpye: 3702355150-hoangtu@ubot.vn-T9- So hoa don tren TCT: 45
+2022-12-20 23:55:12.310136 - C tpye: 3702355150-hoangtu@ubot.vn-T10- So hoa don tren TCT: 64
+2022-12-20 23:56:48.513034 - K tpye: 3702355150-hoangtu@ubot.vn-T10- So hoa don tren TCT: 51
+2022-12-20 23:58:50.813824 - C tpye: 3702355150-hoangtu@ubot.vn-T11- So hoa don tren TCT: 65
+2022-12-21 00:00:45.593252 - K tpye: 3702355150-hoangtu@ubot.vn-T11- So hoa don tren TCT: 49
+2022-12-21 00:02:35.656362 - C tpye: 3702355150-hoangtu@ubot.vn-T12- So hoa don tren TCT: 39
+2022-12-21 00:03:16.122390 - K tpye: 3702355150-hoangtu@ubot.vn-T12- So hoa don tren TCT: 10
+2022-12-21 00:03:32.976279 - C tpye: 0305089034-thamnguyen@ubot.vn-T12- So hoa don tren TCT: 6
+2022-12-21 00:03:45.685600 - K tpye: 0305089034-thamnguyen@ubot.vn-T12- So hoa don tren TCT: 1
+2022-12-21 00:04:06.605357 - C tpye: 0312149527-TrangvoFRP@ubot.vn-T10- So hoa don tren TCT: 15
+2022-12-21 00:04:40.250705 - K tpye: 0312149527-TrangvoFRP@ubot.vn-T10- So hoa don tren TCT: 16
+2022-12-21 00:05:09.946179 - C tpye: 0312149527-TrangvoFRP@ubot.vn-T11- So hoa don tren TCT: 17
+2022-12-21 00:05:32.701410 - K tpye: 0312149527-TrangvoFRP@ubot.vn-T11- So hoa don tren TCT: 12
+2022-12-21 00:05:58.731439 - C tpye: 0312149527-TrangvoFRP@ubot.vn-T12- So hoa don tren TCT: 11
+z2022-12-21 00:06:40.014559 - C tpye: 0303889733-TrangvoFRP@ubot.vn-T10- No inv
+2022-12-21 00:07:08.471824 - K tpye: 0303889733-TrangvoFRP@ubot.vn-T10- So hoa don tren TCT: 1
+2022-12-21 00:07:15.377495 - C tpye: 0303889733-TrangvoFRP@ubot.vn-T11- No inv
+2022-12-21 00:07:24.211400 - K tpye: 0303889733-TrangvoFRP@ubot.vn-T11- No inv
+2022-12-21 00:07:32.550586 - C tpye: 0303889733-TrangvoFRP@ubot.vn-T12- No inv
+2022-12-21 00:07:56.042759 - K tpye: 0303889733-TrangvoFRP@ubot.vn-T12- No inv
+2022-12-21 00:08:18.000273 - C tpye: 0315493849-TrangvoFRP@ubot.vn-T10- No inv
+2022-12-21 00:08:26.694215 - K tpye: 0315493849-TrangvoFRP@ubot.vn-T10- So hoa don tren TCT: 1
+2022-12-21 00:09:08.507012 - C tpye: 0315493849-TrangvoFRP@ubot.vn-T11- No inv
+2022-12-21 00:09:34.907491 - K tpye: 0315493849-TrangvoFRP@ubot.vn-T11- So hoa don tren TCT: 4
+2022-12-21 00:09:43.394894 - C tpye: 0315493849-TrangvoFRP@ubot.vn-T12- No inv
+2022-12-21 00:10:16.964386 - K tpye: 0315493849-TrangvoFRP@ubot.vn-T12- So hoa don tren TCT: 1
+2022-12-21 00:10:24.657291 - C tpye: 0312031099-TrangvoFRP@ubot.vn-T10- So hoa don tren TCT: 1
