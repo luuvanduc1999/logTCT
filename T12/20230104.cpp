c67f216fa931818087ea14db40be57e443a6ca39
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2023-01-04 10:30:20 +0700

    Create 20230104.cpp

---
+++ T12/20230104.cpp
@@ -0,0 +1,6 @@
+2023-01-04 10:22:45.695869 - C tpye: 0109665281-quangminh@ubot.vn-2022T12- So hoa don tren TCT: 24
+2023-01-04 10:23:10.075635 - K tpye: 0109665281-quangminh@ubot.vn-2022T12- So hoa don tren TCT: 8
+2023-01-04 10:23:31.862209 - C tpye: 2400930356-quangminh@ubot.vn-2022T12- So hoa don tren TCT: 7
+2023-01-04 10:23:39.243510 - K tpye: 2400930356-quangminh@ubot.vn-2022T12- No inv
+2023-01-04 10:24:10.944641 - C tpye: 0110160447-quangminh@ubot.vn-2022T12- So hoa don tren TCT: 6
+2023-01-04 10:24:16.973491 - K tpye: 0110160447-quangminh@ubot.vn-2022T12- No inv
