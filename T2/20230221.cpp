db94799549515307cb0f3142c4b24d1f0951e0ea
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2023-02-22 09:15:15 +0700

    Create 20230221.cpp

---
+++ T2/20230221.cpp
@@ -0,0 +1,114 @@
+
+_______________DANG NHAP THAT BAI_______________
+2023-02-22 08:07:31.738764 - C tpye: 0109185574 - hinori@ubot.vn - 2023T1 - DANG NHAP THAT BAI: SAI MAT KHAU
+2023-02-22 08:07:38.755036 - K tpye: 0109185574 - hinori@ubot.vn - 2023T1 - DANG NHAP THAT BAI: SAI MAT KHAU
+
+_______________DAILY_______________
+
+_______________tpye_______________
+2023-02-22 07:32:21.168486 - C tpye: 0202048276 - vinaship@ubot.vn - 2022T1 - So hoa don tren TCT: 29
+2023-02-22 07:32:42.430417 - K tpye: 0202048276 - vinaship@ubot.vn - 2022T1 - So hoa don tren TCT: 2
+2023-02-22 07:33:09.815036 - C tpye: 0202048276 - vinaship@ubot.vn - 2022T2 - So hoa don tren TCT: 18
+2023-02-22 07:33:30.446337 - K tpye: 0202048276 - vinaship@ubot.vn - 2022T2 - So hoa don tren TCT: 2
+2023-02-22 07:34:06.837326 - C tpye: 0202048276 - vinaship@ubot.vn - 2022T3 - So hoa don tren TCT: 26
+2023-02-22 07:34:34.788262 - K tpye: 0202048276 - vinaship@ubot.vn - 2022T3 - So hoa don tren TCT: 2
+2023-02-22 07:35:06.306060 - C tpye: 0202048276 - vinaship@ubot.vn - 2022T4 - So hoa don tren TCT: 30
+2023-02-22 07:35:49.035662 - K tpye: 0202048276 - vinaship@ubot.vn - 2022T4 - So hoa don tren TCT: 6
+2023-02-22 07:36:40.577709 - C tpye: 0202048276 - vinaship@ubot.vn - 2022T5 - So hoa don tren TCT: 28
+2023-02-22 07:37:14.904663 - K tpye: 0202048276 - vinaship@ubot.vn - 2022T5 - So hoa don tren TCT: 7
+2023-02-22 07:38:15.548029 - C tpye: 0202048276 - vinaship@ubot.vn - 2022T6 - So hoa don tren TCT: 38
+2023-02-22 07:38:46.767977 - K tpye: 0202048276 - vinaship@ubot.vn - 2022T6 - So hoa don tren TCT: 5
+2023-02-22 07:39:00.605165 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T1 - No inv
+2023-02-22 07:39:05.752594 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T2 - No inv
+2023-02-22 07:39:58.467046 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T3 - So hoa don tren TCT: 8
+2023-02-22 07:40:13.450476 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T4 - No inv
+2023-02-22 07:40:50.817730 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T5 - So hoa don tren TCT: 2
+2023-02-22 07:41:08.511655 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T6 - No inv
+2023-02-22 07:41:21.655914 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T7 - So hoa don tren TCT: 1
+2023-02-22 07:41:31.102672 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T8 - No inv
+2023-02-22 07:41:47.527142 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T9 - No inv
+2023-02-22 07:42:11.037015 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T10 - So hoa don tren TCT: 1
+2023-02-22 07:42:33.953876 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T11 - So hoa don tren TCT: 1
+2023-02-22 07:42:53.034523 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2022T12 - So hoa don tren TCT: 12
+2023-02-22 07:43:20.794894 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2023T1 - So hoa don tren TCT: 1
+2023-02-22 07:44:08.785715 - C tpye: 2500630250 - xaydungbaytung@ubot.vn - 2023T2 - So hoa don tren TCT: 4
+2023-02-22 07:44:33.851583 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2021T11 - No inv
+2023-02-22 07:45:01.424058 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2021T12 - So hoa don tren TCT: 4
+2023-02-22 07:45:34.583017 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T1 - So hoa don tren TCT: 16
+2023-02-22 07:45:45.182259 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T2 - So hoa don tren TCT: 3
+2023-02-22 07:46:05.444453 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T3 - So hoa don tren TCT: 8
+2023-02-22 07:46:34.795341 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T4 - So hoa don tren TCT: 18
+2023-02-22 07:47:31.420714 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T5 - So hoa don tren TCT: 16
+2023-02-22 07:48:02.088274 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T6 - So hoa don tren TCT: 16
+2023-02-22 07:48:43.781741 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T7 - So hoa don tren TCT: 40
+2023-02-22 07:49:21.216571 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T8 - So hoa don tren TCT: 27
+2023-02-22 07:49:52.658532 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T9 - So hoa don tren TCT: 25
+2023-02-22 07:50:15.690277 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T10 - So hoa don tren TCT: 18
+2023-02-22 07:51:12.458714 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T11 - So hoa don tren TCT: 8
+2023-02-22 07:51:49.378647 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2022T12 - So hoa don tren TCT: 25
+2023-02-22 07:52:20.225526 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2023T1 - So hoa don tren TCT: 13
+2023-02-22 07:52:54.248109 - C tpye: 2500563406 - xaydungbaytung@ubot.vn - 2023T2 - So hoa don tren TCT: 5
+2023-02-22 07:53:09.428435 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T1 - So hoa don tren TCT: 1
+2023-02-22 07:53:21.319001 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T2 - No inv
+2023-02-22 07:53:33.262413 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T3 - No inv
+2023-02-22 07:53:46.600488 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T4 - So hoa don tren TCT: 1
+2023-02-22 07:54:03.666541 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T5 - So hoa don tren TCT: 2
+2023-02-22 07:54:20.716975 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T6 - So hoa don tren TCT: 1
+2023-02-22 07:54:41.760161 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T7 - So hoa don tren TCT: 4
+2023-02-22 07:55:11.964640 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T8 - So hoa don tren TCT: 6
+2023-02-22 07:55:42.754325 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T9 - So hoa don tren TCT: 6
+2023-02-22 07:56:10.641990 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T10 - So hoa don tren TCT: 6
+2023-02-22 07:56:33.119641 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T11 - So hoa don tren TCT: 7
+2023-02-22 07:57:10.351295 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2022T12 - So hoa don tren TCT: 25
+2023-02-22 07:57:28.042408 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2023T1 - So hoa don tren TCT: 5
+2023-02-22 07:57:56.647689 - C tpye: 2500509663 - xaydungbaytung@ubot.vn - 2023T2 - So hoa don tren TCT: 6
+2023-02-22 07:58:59.049592 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T1 - So hoa don tren TCT: 4
+2023-02-22 07:59:08.028451 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T2 - No inv
+2023-02-22 07:59:34.749480 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T3 - So hoa don tren TCT: 5
+2023-02-22 08:00:10.988996 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T4 - So hoa don tren TCT: 8
+2023-02-22 08:00:47.250442 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T5 - So hoa don tren TCT: 10
+2023-02-22 08:01:45.727425 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T6 - So hoa don tren TCT: 29
+2023-02-22 08:02:15.498536 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T7 - So hoa don tren TCT: 30
+2023-02-22 08:02:57.146989 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T8 - So hoa don tren TCT: 41
+2023-02-22 08:04:05.214256 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T9 - So hoa don tren TCT: 28
+2023-02-22 08:04:39.929409 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T10 - So hoa don tren TCT: 41
+2023-02-22 08:05:32.318810 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T11 - So hoa don tren TCT: 34
+2023-02-22 08:06:26.745402 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2022T12 - So hoa don tren TCT: 83
+2023-02-22 08:06:48.647838 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2023T1 - So hoa don tren TCT: 14
+2023-02-22 08:07:23.317228 - C tpye: 2500360318 - xaydungbaytung@ubot.vn - 2023T2 - So hoa don tren TCT: 18
+2023-02-22 08:08:28.865051 - C tpye: 0314194490 - quocanh@ubot.vn - 2023T2 - So hoa don tren TCT: 4
+2023-02-22 08:09:03.543468 - K tpye: 0314194490 - quocanh@ubot.vn - 2023T2 - So hoa don tren TCT: 2
+2023-02-22 08:10:36.261501 - C tpye: 0103014205 - elematec@ubot.vn - 2022T1 - So hoa don tren TCT: 159
+2023-02-22 08:11:22.884061 - K tpye: 0103014205 - elematec@ubot.vn - 2022T1 - So hoa don tren TCT: 9
+2023-02-22 08:13:04.966491 - C tpye: 0103014205 - elematec@ubot.vn - 2022T2 - So hoa don tren TCT: 206
+2023-02-22 08:13:34.698515 - K tpye: 0103014205 - elematec@ubot.vn - 2022T2 - So hoa don tren TCT: 16
+2023-02-22 08:15:10.972230 - C tpye: 0103014205 - elematec@ubot.vn - 2022T3 - So hoa don tren TCT: 242
+2023-02-22 08:15:51.775554 - K tpye: 0103014205 - elematec@ubot.vn - 2022T3 - So hoa don tren TCT: 12
+2023-02-22 08:18:04.099066 - C tpye: 0103014205 - elematec@ubot.vn - 2022T4 - So hoa don tren TCT: 251
+2023-02-22 08:18:49.663445 - K tpye: 0103014205 - elematec@ubot.vn - 2022T4 - So hoa don tren TCT: 24
+2023-02-22 08:20:52.858101 - C tpye: 0103014205 - elematec@ubot.vn - 2022T5 - So hoa don tren TCT: 264
+2023-02-22 08:21:41.894509 - K tpye: 0103014205 - elematec@ubot.vn - 2022T5 - So hoa don tren TCT: 67
+2023-02-22 08:24:29.072276 - C tpye: 0103014205 - elematec@ubot.vn - 2022T6 - So hoa don tren TCT: 353
+2023-02-22 08:25:19.083251 - K tpye: 0103014205 - elematec@ubot.vn - 2022T6 - So hoa don tren TCT: 105
+2023-02-22 08:27:08.054786 - C tpye: 0103014205 - elematec@ubot.vn - 2022T7 - So hoa don tren TCT: 279
+2023-02-22 08:27:53.649629 - K tpye: 0103014205 - elematec@ubot.vn - 2022T7 - So hoa don tren TCT: 105
+2023-02-22 08:30:14.844679 - C tpye: 0103014205 - elematec@ubot.vn - 2022T8 - So hoa don tren TCT: 300
+2023-02-22 08:31:00.955620 - K tpye: 0103014205 - elematec@ubot.vn - 2022T8 - So hoa don tren TCT: 99
+2023-02-22 08:32:54.310773 - C tpye: 0103014205 - elematec@ubot.vn - 2022T9 - So hoa don tren TCT: 271
+2023-02-22 08:33:36.531203 - K tpye: 0103014205 - elematec@ubot.vn - 2022T9 - So hoa don tren TCT: 95
+2023-02-22 08:35:26.293114 - C tpye: 0103014205 - elematec@ubot.vn - 2022T10 - So hoa don tren TCT: 256
+2023-02-22 08:36:24.866362 - K tpye: 0103014205 - elematec@ubot.vn - 2022T10 - So hoa don tren TCT: 98
+2023-02-22 08:38:19.650208 - C tpye: 0103014205 - elematec@ubot.vn - 2022T11 - So hoa don tren TCT: 258
+2023-02-22 08:39:21.273239 - K tpye: 0103014205 - elematec@ubot.vn - 2022T11 - So hoa don tren TCT: 131
+2023-02-22 08:40:54.727298 - C tpye: 0103014205 - elematec@ubot.vn - 2022T12 - So hoa don tren TCT: 274
+2023-02-22 08:42:23.496193 - K tpye: 0103014205 - elematec@ubot.vn - 2022T12 - So hoa don tren TCT: 152
+2023-02-22 08:43:23.565182 - C tpye: 0103014205 - elematec@ubot.vn - 2023T1 - So hoa don tren TCT: 175
+2023-02-22 08:44:00.470210 - K tpye: 0103014205 - elematec@ubot.vn - 2023T1 - So hoa don tren TCT: 98
+2023-02-22 08:44:16.250812 - C tpye: 0309759441 - quanact@ubot.vn - 2022T12 - So hoa don tren TCT: 90
+2023-02-22 08:45:04.454971 - K tpye: 0309759441 - quanact@ubot.vn - 2022T12 - So hoa don tren TCT: 25
+2023-02-22 08:45:22.544713 - C tpye: 0103014205 - elematec@ubot.vn - 2023T2 - So hoa don tren TCT: 152
+2023-02-22 08:45:41.489751 - C tpye: 0309759441 - quanact@ubot.vn - 2023T1 - So hoa don tren TCT: 22
+2023-02-22 08:46:05.127460 - K tpye: 0103014205 - elematec@ubot.vn - 2023T2 - So hoa don tren TCT: 75
+2023-02-22 08:46:09.028551 - K tpye: 0309759441 - quanact@ubot.vn - 2023T1 - So hoa don tren TCT: 24
+2023-02-22 08:46:53.624185 - C tpye: 0309759441 - quanact@ubot.vn - 2023T2 - So hoa don tren TCT: 14
+2023-02-22 08:47:21.882455 - K tpye: 0309759441 - quanact@ubot.vn - 2023T2 - So hoa don tren TCT: 4
